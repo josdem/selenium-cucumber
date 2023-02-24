001f3de6a0b97f9d4915378d481fac83609ebd6e
josdem <joseluis.delacruz@gmail.com>
2023-02-23 19:18:36 -0500

    Adding test runner structure

--- src/test/java/com/josdem/selenium/cucumber/TestRunner.java
+++ src/test/java/com/josdem/selenium/cucumber/TestRunner.java
@@ -1,3 +1,15 @@
 package com.josdem.selenium.cucumber;
 
-public class TestRunner {}
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+    plugin = {
+      "pretty",
+      "html:target/cucumber-reports/cucumber.html",
+      "json:target/cucumber-reports/cucumber.json"
+    },
+    features = {"src/test/resources/features"},
+    glue = {"com.josdem.selenium.cucumber.steps"},
+    tags = "@smoke")
+public class TestRunner extends AbstractTestNGCucumberTests {}
